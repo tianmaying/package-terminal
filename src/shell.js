4b4236f346f9380bd386b0ec4f8497ff76595be0
Ruici Luo <luoruici@gmail.com>
2016-06-28 12:06:03 +0800

    using socket.io

--- src/shell.js
+++ src/shell.js
@@ -1,7 +1,7 @@
 var _ = codebox.require("hr.utils");
 var Class = codebox.require("hr.class");
 var hash = codebox.require("utils/hash");
-var Socket = codebox.require("core/socket");
+var Socket = codebox.require("socket.io-client");
 
 var logging = codebox.require("hr.logger")("terminal");
 
@@ -24,32 +24,47 @@ var Shell = Class.extend({
             return this;
         }
 
-        this.socket = new Socket({
-            service: "terminal"
-        });
+        this.socket = Socket.connect();
 
-        this.listenTo(this.socket, "close", function() {
-            this.trigger("disconnect");
-        });
-
-        this.listenTo(this.socket, "do:output", function(data) {
-            // this.trigger("data", decodeURIComponent(window.atob(data)));
-            this.trigger("data", data);
-        });
-
-        this.listenTo(this.socket, "open", function() {
-            this.trigger("connect");
-
-            this.socket.do('open', {
-                "shellId": that.shellId,
-                "opts": {
-                    "rows": 80,
-                    "columns": 24,
-                    "id": that.shellId,
-                    "cwd": that.options.cwd
-                }
+        this.socket.on('connect', function() {
+            that.trigger('connect');
+            that.socket.emit("term.open", {
+                id: that.shellId,
+                cols: 80,
+                rows: 24
             });
         });
+
+        this.socket.on('term.output', function(data) {
+            that.trigger(data.output);
+        });
+
+        this.socket.on('disconnect', function() {
+            that.trigger('disconnect');
+        });
+
+        // this.listenTo(this.socket, "close", function() {
+        //     this.trigger("disconnect");
+        // });
+        //
+        // this.listenTo(this.socket, "do:output", function(data) {
+        //     // this.trigger("data", decodeURIComponent(window.atob(data)));
+        //     this.trigger("data", data);
+        // });
+        //
+        // this.listenTo(this.socket, "open", function() {
+        //     this.trigger("connect");
+        //
+        //     this.socket.do('open', {
+        //         "shellId": that.shellId,
+        //         "opts": {
+        //             "rows": 80,
+        //             "columns": 24,
+        //             "id": that.shellId,
+        //             "cwd": that.options.cwd
+        //         }
+        //     });
+        // });
     },
 
     /*
@@ -57,7 +72,7 @@ var Shell = Class.extend({
      */
     disconnect: function() {
         if (this.socket != null) {
-            this.socket.close();
+            this.socket.disconnect();
         }
         return this;
     },
@@ -67,7 +82,11 @@ var Shell = Class.extend({
      */
     write: function(buf) {
         if (this.socket != null) {
-            this.socket.do("input", hash.btoa(buf));
+            // this.socket.do("input", hash.btoa(buf));
+            this.socket.emit("term:input", {
+                id: this.shellId,
+                input: buf
+            });
         }
         return this;
     },
@@ -77,23 +96,24 @@ var Shell = Class.extend({
      */
     resize: function(w, h) {
         if (this.socket != null) {
-            this.socket.do("resize", {
-                "rows": h,
-                "columns": w
+            this.socket.emit("term.resize", {
+                "id": this.shellId,
+                "cols": w,
+                "rows": h
             });
         }
         return this;
-    },
+    }
 
     /*
      *  Force destroy the shell
      */
-    forceDestroy: function() {
-        if (this.socket != null) {
-            this.socket.do("destroy");
-        }
-        return this;
-    }
+    // forceDestroy: function() {
+    //     if (this.socket != null) {
+    //         this.socket.do("destroy");
+    //     }
+    //     return this;
+    // }
 });
 
 module.exports = Shell;
